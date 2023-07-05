6d0469334295126e8fee721d5cfb93fe09d8e117
Maciej Uberna <maciej.uberna@gmail.com>
2023-07-05 13:19:51 +0200

    Added #number and % at each bar for comparative analysis

--- config/config_data_manipulator.py
+++ config/config_data_manipulator.py
@@ -9,3 +9,8 @@ class DataManipulator:
         d[col_name] = d[col_name] * 100
         d[col_name] = d[col_name].round().astype(int)
         return d
+
+    @staticmethod
+    def extractBarPlotNumericalData(d: pd, groupBy: str="", col_name: str=""):
+        d = d.groupby([groupBy]).size().reset_index(name = col_name)
+        return d
--- data_display/display_corpora_cmp.py
+++ data_display/display_corpora_cmp.py
@@ -38,8 +38,8 @@ class ComparativeCorporaSimple:
     def __init__(self, data_dic: dict[str,pd.DataFrame()], anCfg: dict[str,str]):
         options, selected = self.__filterInterface(anCfg=anCfg)
         units = st.radio("Choose: ", ("Percentage","Number"),label_visibility='collapsed')
-        fig, ax = plt.subplots(4, 2, figsize=(13,40), sharex=True)
-        fig.subplots_adjust(left=-0.1, bottom=0.1, right=1.2, top=0.9, wspace=0.2, hspace=0.2)
+        fig, ax = plt.subplots(4, 2, figsize=(10,45), sharex=True)
+        fig.subplots_adjust(left=-1, bottom=0.1, right=1.2, top=0.9, wspace=0.2, hspace=0.2)
         sns.set(font_scale=2)
         four_dim = [[0,0],[0,1],[1,0],[1,1],[2,0],[2,1],[3,0],[3,1]]
         if len(data_dic) > 1:
@@ -48,26 +48,29 @@ class ComparativeCorporaSimple:
                     data = pairs[1]
                     if len(data) > 0:
                         data = data.loc[data[selected].isin(options)]
-                        x = sns.countplot(data = data.sort_values(by = [selected]), y = selected, ax=ax[four_dim[ctr][0],four_dim[ctr][1]],
+                        axTmp = ax[four_dim[ctr][0],four_dim[ctr][1]]
+                        x = sns.countplot(data = data.sort_values(by = [selected]), y = selected, ax=axTmp,
                             palette = DataProvider.getEthosColors())
+                        x.bar_label(x.containers[0], fmt='#%d')
                         x.grid(b=True, which='major', color='black', linewidth=0.075)
                         x.set(title=pairs[0])
                         x.set_xlabel("Number",fontsize=20)
-                        x.set_ylabel(anCfg['anName'], fontsize=20)
+                        x.set_ylabel("", fontsize=20)
                         x.tick_params(labelsize=15)
             elif units == "Percentage":
                 for ctr, pairs in enumerate(data_dic.items()):
                     data = pairs[1]
                     if len(data) > 0:
-                        denominator = len(data)
                         data = data.loc[data[selected].isin(options)]
+                        denominator = len(data)
                         data = DataManipulator.extractBarPlotPercentageData(d=data, denominator=denominator, groupBy=selected, col_name='Percentage')
                         z = sns.barplot(data = data, y = selected, x = 'Percentage', ax=ax[four_dim[ctr][0],four_dim[ctr][1]], 
                             palette = DataProvider.getEthosColors())
+                        z.bar_label(z.containers[0], fmt='%d%%')
                         z.grid(b=True, which='major', color='black', linewidth=0.075)
                         z.set(title=pairs[0])
                         z.set_xlabel("Percentage",fontsize=20)
-                        z.set_ylabel(anCfg['anName'], fontsize=20)
+                        z.set_ylabel("", fontsize=20)
                         z.tick_params(labelsize=20)       
             st.pyplot(fig=fig)
         else:
