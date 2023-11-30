283ada5130d2cb520fc4a0c5f85193898831867d
Maciej Uberna <maciej.uberna@gmail.com>
2023-11-30 15:23:03 +0100

    Added PolarIs1 to 3D charts - to Total - BUG FIX

--- submenus/_3D_PSP_corpus.py
+++ submenus/_3D_PSP_corpus.py
@@ -70,7 +70,7 @@ class _3D_PSP_corpus:
 
     def __prepCorpora_and_DynRephType(self) -> Dict[str, Dict[str, int]]:
         corpus3Ddic = {
-             'Total': {'US2016redditD1','US2016redditR1','US2016redditG1','US2016tvD1','US2016tvR1','US2016tvG1','Hansard'},
+             'Total': {'US2016redditD1','US2016redditR1','US2016redditG1','US2016tvD1','US2016tvR1','US2016tvG1','Hansard','PolarIs1vacc'},
              'SocialMedia': {'US2016redditD1','US2016redditR1','US2016redditG1','PolarIs1vacc'},
              'Media': {'US2016tvD1','US2016tvR1','US2016tvG1'},
              'F2F': {'Hansard'}
--- submenus/three_d_corpus.py
+++ submenus/three_d_corpus.py
@@ -102,7 +102,7 @@ class ThreeDCorpusMenu:
 
     def __prepCorpora_and_DynRephType(self, bothEthosPathos: bool=False) -> Dict[str, Dict[str, int]]:
         corpus3Ddic = {
-             'Total': {'US2016redditD1','US2016redditR1','US2016redditG1','US2016tvD1','US2016tvR1','US2016tvG1','Hansard'},
+             'Total': {'US2016redditD1','US2016redditR1','US2016redditG1','US2016tvD1','US2016tvR1','US2016tvG1','Hansard','PolarIs1vacc'},
              'SocialMedia': {'US2016redditD1','US2016redditR1','US2016redditG1','PolarIs1vacc'},
              'Media': {'US2016tvD1','US2016tvR1','US2016tvG1'},
              'F2F': {'Hansard'}
