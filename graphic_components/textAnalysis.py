c7e83455c64c1ec1cf0455fc6588d396d7890fa4
Maciej Uberna <maciej.uberna@gmail.com>
2024-03-19 16:40:54 +0100

    Changed sr.dataframe - so that it is compatible with streamlit 1.21

--- graphic_components/textAnalysis.py
+++ graphic_components/textAnalysis.py
@@ -24,4 +24,5 @@ class Cases2(SuperTextComponent):
 
     def dataDisplay(self, df: Any, title: str):
         stylesed = self.getTextObj(df,title)
-        st.dataframe(stylesed, height=800, column_config={self._cf['categoriesColumn']: None})
+        #st.dataframe(stylesed, height=800, column_config={self._cf['categoriesColumn']: None})
+        st.dataframe(stylesed, height=800)
