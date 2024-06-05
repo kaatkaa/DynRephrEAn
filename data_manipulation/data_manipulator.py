c68fcdfd865aa1023bcdfcff5d15f8aed1a450b8
Maciej Uberna <maciej.uberna@gmail.com>
2024-06-05 17:14:06 +0200

    python -> python3

--- data_manipulation/data_manipulator.py
+++ data_manipulation/data_manipulator.py
@@ -8,7 +8,7 @@ import spacy
 
 @st.cache_resource
 def download_en_core_web_sm():
-    subprocess.run(["python", "-m", "spacy", "download", "en_core_web_sm"])
+    subprocess.run(["python3", "-m", "spacy", "download", "en_core_web_sm"])
 download_en_core_web_sm()
 nlp = spacy.load("en_core_web_sm")
 
